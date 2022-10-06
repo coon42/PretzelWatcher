10d9fcb18cded7fd798d1c79db254a0ebdc42743
coon <coon@mailbox.org>
2022-10-06 02:19:35 +0200

    main.cpp: implement basic FileWatcher class

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <dinput.h>
 
 #include "Logger.h"
+#include "stringhelpers.h"
 
 using namespace std;
 
@@ -125,11 +126,76 @@ void PretzelProcess::stopMusic() const {
 }
 
 //--------------------------------------------------------------------------------------------------------------
+// FileWatcher
+//--------------------------------------------------------------------------------------------------------------
+
+class FileWatcher {
+public:
+  FileWatcher(const string& filePath) : filePath_(filePath) { };
+
+  bool waitForFileChange();
+
+private:
+  const string filePath_;
+};
+
+bool FileWatcher::waitForFileChange() {
+  char pDrive[4];
+  char pDir[_MAX_DIR];
+  char pFile[_MAX_FNAME];
+
+  if (errno_t err = _splitpath_s(filePath_.c_str(), pDrive, 4, pDir, _MAX_DIR, pFile, _MAX_FNAME, NULL, NULL))
+    return false;
+
+  const string dirPath = __("%s%s", pDrive, pDir);
+
+  while (true) {
+    const HANDLE hDir = CreateFileA(dirPath.c_str(),
+      GENERIC_READ,
+      FILE_SHARE_READ,
+      NULL,
+      OPEN_EXISTING,
+      FILE_FLAG_BACKUP_SEMANTICS,
+      NULL
+    );
+
+    unsigned char pBuf[sizeof(FILE_NOTIFY_INFORMATION) + _MAX_PATH]{0};
+
+    DWORD bytesReturned = 0;
+
+    if (ReadDirectoryChangesW(hDir, pBuf, sizeof(pBuf), FALSE, FILE_NOTIFY_CHANGE_ATTRIBUTES, &bytesReturned, NULL, NULL) == 0) {
+      Logger::logError("ERROR: ReadDirectoryChangesW function failed.\n");
+      return false;
+    }
+
+    if (bytesReturned == 0) {
+      Logger::logError("ERROR: Cannot get file info.\n");
+      return false;
+    }
+
+    CloseHandle(hDir);
+
+    const FILE_NOTIFY_INFORMATION* pFileInfo = reinterpret_cast<const FILE_NOTIFY_INFORMATION*>(pBuf);
+    char pMbFileName[_MAX_PATH];
+
+    const int len = WideCharToMultiByte(CP_UTF8, WC_ERR_INVALID_CHARS, pFileInfo->FileName, -1, pMbFileName,
+        sizeof(pMbFileName), NULL, NULL);
+
+    const string notifiedFilePath = dirPath + pMbFileName;
+
+    if (notifiedFilePath == filePath_)
+      return true;
+  }
+}
+
+//--------------------------------------------------------------------------------------------------------------
 // PretzelWatcherApp
 //--------------------------------------------------------------------------------------------------------------
 
 class PretzelWatcherApp {
 public:
+  PretzelWatcherApp();
+
   int run();
 
 private:
@@ -140,11 +206,18 @@ private:
   void stopWorkerThread();
   bool workerThreadIsRunning() const;
   static DWORD WINAPI workerThread(LPVOID lpParam);
+
+  FileWatcher watcher_;
 };
 
 //--------------------------------------------------------------------------------------------------------------
 // PretzelWatcherApp
 //--------------------------------------------------------------------------------------------------------------
+
+PretzelWatcherApp::PretzelWatcherApp()
+    : watcher_("C:\\Users\\Public\\Documents\\current_song.txt") {
+
+}
 
 int PretzelWatcherApp::run() {
   printf("--- Pretzel Watcher ---\n\n");
