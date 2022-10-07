bd4cd0e746a5c40650e513e7859000b357a0915c
coon <coon@mailbox.org>
2022-10-07 23:53:04 +0200

    build v1.1

--- main.cpp
+++ main.cpp
@@ -163,7 +163,7 @@ DWORD WINAPI PretzelWatcherApp::workerThread(LPVOID lpParam) {
 //--------------------------------------------------------------------------------------------------------------
 
 int main(int argc, char* ppArgv[]) {
-  printf("--- Pretzel Watcher v1.0 ---\n\n");
+  printf("--- Pretzel Watcher v1.1 ---\n\n");
 
   if (argc != 3) {
     printf("Usage: PretzelWatcher.exe <Track info file> <Restart interval minutes>\n");
