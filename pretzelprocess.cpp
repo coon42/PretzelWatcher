31081df545de0d5f406092e57bb5e443e3d87546
coon <coon@mailbox.org>
2022-10-07 21:32:21 +0200

    main.cpp: lay out PretzelProcess to pretzelprocess .cpp + ..h

---
+++ pretzelprocess.cpp
@@ -0,0 +1,117 @@
+#define DIRECTINPUT_VERSION 0x0800
+#include <dinput.h>
+
+#include "Logger.h"
+
+#include "pretzelprocess.h"
+
+//--------------------------------------------------------------------------------------------------------------
+// PretzelProcess
+//--------------------------------------------------------------------------------------------------------------
+
+PretzelProcess::PretzelProcess(const std::string& title, const std::string& className)
+    : title_(title), className_(className), exePath_(getExePath()) {
+
+}
+
+HWND PretzelProcess::getHwnd() const {
+  return FindWindowA(className_.c_str(), title_.c_str());
+}
+
+DWORD PretzelProcess::getProcessId() const {
+  const HWND hWnd = getHwnd();
+
+  if (hWnd == INVALID_HANDLE_VALUE)
+    return 0;
+
+  DWORD processId = 0;
+  GetWindowThreadProcessId(hWnd, &processId);
+
+  return processId;
+}
+
+std::string PretzelProcess::getExePath() const {
+  HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, getProcessId());
+
+  if (!hProc)
+    return "";
+
+  char pExePath[MAX_PATH];
+  DWORD bufferSize = sizeof(pExePath);
+
+  if (QueryFullProcessImageNameA(hProc, 0, pExePath, &bufferSize) == 0)
+    return "";
+
+  CloseHandle(hProc);
+
+  return pExePath;
+}
+
+bool PretzelProcess::isRunning() const {
+  const DWORD processId = getProcessId();
+
+  if (!processId)
+    return false;
+
+  return true;
+}
+
+bool PretzelProcess::launch() {
+  STARTUPINFOA si;
+  GetStartupInfoA(&si);
+
+  PROCESS_INFORMATION pi;
+  char* pCmdLine = const_cast<char*>(static_cast<const char*>(exePath_.c_str()));
+
+  if (CreateProcessA(NULL, pCmdLine, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi) == 0)
+    return false;
+
+  CloseHandle(pi.hProcess);
+  CloseHandle(pi.hThread);
+
+  return true;
+}
+
+bool PretzelProcess::close() {
+  HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, getProcessId());
+
+  if (!hProc)
+    return false;
+
+  if (TerminateProcess(hProc, 0) == 0) {
+    CloseHandle(hProc);
+    return false;
+  }
+
+  CloseHandle(hProc);
+
+  return true;
+}
+
+void PretzelProcess::sendInput(BYTE vKey, BYTE bScan, DWORD dwFlags) const {
+  INPUT ip{0};
+  ip.type = INPUT_KEYBOARD;
+  ip.ki.wScan = bScan;
+  ip.ki.time = 0;
+  ip.ki.dwExtraInfo = 0;
+  ip.ki.wVk = vKey;
+  ip.ki.dwFlags = dwFlags;
+  SendInput(1, &ip, sizeof(INPUT));
+}
+
+void PretzelProcess::pressKey(BYTE vKey, BYTE bScan) const {
+  sendInput(vKey, bScan, 0);
+  sendInput(vKey, bScan, KEYEVENTF_KEYUP);
+}
+
+void PretzelProcess::playMusic() const {
+  Logger::log("Start playing music...\n");
+
+  pressKey(VK_MEDIA_PLAY_PAUSE, DIK_PLAYPAUSE);
+}
+
+void PretzelProcess::stopMusic() const {
+  Logger::log("Stop playing music...\n");
+
+  pressKey(VK_MEDIA_STOP, DIK_MEDIASTOP);
+}
---
+++ pretzelprocess.h
@@ -0,0 +1,36 @@
+#ifndef PRETZELPROCESS_H
+#define PRETZELPROCESS_H
+
+#include <string>
+#include <windows.h>
+
+//--------------------------------------------------------------------------------------------------------------
+// PretzelProcess
+//--------------------------------------------------------------------------------------------------------------
+
+class PretzelProcess {
+public:
+  PretzelProcess(const std::string& title, const std::string& className);
+
+  DWORD getProcessId() const;
+  const std::string& exePath() const { return exePath_; }
+
+  bool isRunning() const;
+  bool launch();
+  bool close();
+
+  void playMusic() const;
+  void stopMusic() const;
+
+private:
+  void sendInput(BYTE vKey, BYTE bScan, DWORD dwFlags) const;
+  void pressKey(BYTE vKey, BYTE bScan) const;
+  HWND getHwnd() const;
+  std::string getExePath() const;
+
+  const std::string title_;
+  const std::string className_;
+  const std::string exePath_;
+};
+
+#endif // PRETZELPROCESS_H
